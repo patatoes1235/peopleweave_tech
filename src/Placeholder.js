90ad729e11b43e6c084d70696a6a7678f1dadf9b
patatoes1235 <kzhou12356@gmail.com>
2023-09-18 02:34:19 -0500

    init pt 2

---
+++ src/Placeholder.js
@@ -0,0 +1,19 @@
+export const design2 = {
+    subNumber: 1500,
+    acceptRate: 50,
+    trendTag: [
+        {name: "Neural Network", id: 157},
+        {name: "Data Analysis", id: 131},
+        {name: "fake data 1", id: 121},
+        {name: "fake data 2", id: 121},
+        {name: "fake data 3", id: 121},
+        {name: "fake data 4", id: 121},
+        {name: "fake data 5", id: 121},
+        {name: "fake data 6", id: 121}],
+    awards: [
+        {award: "Best paper", paperTitle: "paper title that is usually pretty long"},
+        {award: "Best paper", paperTitle: "paper title that is usually pretty long"},
+        {award: "Best paper", paperTitle: "paper title that is usually pretty long"}]
+};
+
+export const design1 = {};
---
+++ src/components/Navbar.js
@@ -0,0 +1,43 @@
+import { Box, Flex, HStack} from "@chakra-ui/react";
+
+
+const NavItem = (props, link) => {
+    const { children } = props
+  
+    return (
+      <Box
+        as="a"
+        posx={2}
+        posy={1}
+        rounded={'md'}
+        _hover={{
+          textDecoration: 'none',
+          bg: useColorModeValue('gray.200', 'gray.700'),
+        }}
+        href={link}>
+        {children}
+      </Box>
+    )
+  }
+
+  export default Navbar = () => {
+    <Box bg={useColorModeValue('gray.100', 'gray.900')} px={4}>
+        <Flex h={16} alignItems={'center'} justifyContent={'space-between'}>
+          <IconButton
+            size={'md'}
+            icon={isOpen ? <CloseIcon /> : <HamburgerIcon />}
+            aria-label={'Open Menu'}
+            display={{ md: 'none' }}
+            onClick={isOpen ? onClose : onOpen}
+          />
+          <HStack spacing={8} alignItems={'center'}>
+            <Box>Logo</Box>
+            <HStack as={'nav'} spacing={4} display={{ base: 'none', md: 'flex' }}>
+              {Links.map((link) => (
+                <NavLink key={link}>{link}</NavLink>
+              ))}
+            </HStack>
+          </HStack>
+        </Flex>
+    </Box> 
+  }
