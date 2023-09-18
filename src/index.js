4b662c1a1adba9d2a75da902551cf6c411e6e02f
patatoes1235 <kzhou12356@gmail.com>
2023-09-18 01:42:12 -0500

    init

--- src/index.js
+++ src/index.js
@@ -7,6 +7,7 @@ import reportWebVitals from './reportWebVitals';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
+    <navbar></navbar>
     <App />
   </React.StrictMode>
 );
