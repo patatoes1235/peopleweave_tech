df161dfd2bee8a4bb8b35c8ec7f9724db15d38c9
patatoes1235 <kzhou12356@gmail.com>
2023-09-19 00:04:38 -0500

    change to hashrouter

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import {ChakraProvider, theme,} from '@chakra-ui/react';
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route, HashRouter } from "react-router-dom";
 import { Logo } from './Logo';
 import Navbar from './components/Navbar';
 import Page1 from './pages/Page1';
@@ -10,15 +10,14 @@ function App() {
   return (
     <>
       <ChakraProvider theme={theme}>
+        <HashRouter>
         <Navbar />
-        
-        <BrowserRouter basename={process.env.PUBLIC_URL} >
           <Routes>
             <Route path="/pg1" element={<Page1 />} />
             <Route path="/pg2" element={<Page2 />} />
             <Route path="/" element={<Page1 />} />
           </Routes>
-        </BrowserRouter>
+        </HashRouter>
       </ChakraProvider>
 
     </>
